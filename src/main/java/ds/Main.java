f862dc9ab47fc44ecccbc3f75e2a4b3420e6c83e
adipukr <dipu.kumar@50hertz.in>
2025-07-21 18:17:10 +0530

    chg

--- src/main/java/ds/Main.java
+++ src/main/java/ds/Main.java
@@ -1,13 +1,14 @@
 package ds;
 
-import java.util.HashMap;
-
 public class Main {
 	public static void main(final String[] args) {
-		var m = new HashMap<Integer, Integer>();
-		m.put(1, 100);
-		m.put(2, 200);
-		m.put(3, 300);
-		System.out.println(m);
+		var cq = new CircularQueue(3);
+		cq.enqueue(100);
+		cq.enqueue(200);
+		cq.enqueue(300);
+		System.out.println(cq.size());
+		while (!cq.empty()) {
+			System.out.println(cq.dequeue());
+		}
 	}
 }
